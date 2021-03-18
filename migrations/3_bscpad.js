77ad8be9180ba5db7e30ed4edcec81e4adc9bdbb
chainergar <garmoniart@gmail.com>
2021-03-17 22:52:32 -0400

    Update migrations

--- migrations/3_bscpad.js
+++ migrations/3_bscpad.js
@@ -3,8 +3,9 @@ var BSCPAD = artifacts.require('BSCPAD');
 module.exports = function (deployer, network) {
   if (network === 'test') return;
   deployer.then(function () {
-    return;
-    return deployer.deploy(BSCPAD).then(function (token) {
+    // return;
+    return deployer.deploy(BSCPAD).then(async function (token) {
+      await token.initialize('10000000000000000000000');
       console.log(`BSCPAD is deployed at ${token.address}`);
     });
   });
